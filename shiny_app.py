b1ae4641cdf62cb49c3de9be7f1319f3308c3d71
GabrielZelva <66466481+GabrielZelva@users.noreply.github.com>
2025-11-20 11:25:58 +0100

    The app now integrates the formula
    Works with Spain 2015. Error handling needs implementing, because we don't have all the data.

--- shiny_app.py
+++ shiny_app.py
@@ -1,7 +1,8 @@
-from shared import df
+# from shared import df
 
 from datetime import date
 from shiny import App, render, ui
+import pandas as pd
 
 # User interface (UI) definition
 app_ui = ui.page_fluid(
@@ -13,18 +14,18 @@ app_ui = ui.page_fluid(
     ui.input_numeric("year", "Year of the scanning", date.today().year), 
 
     ui.input_select(
-        "country", "Country", choices=["Ecolandia", "Petrolandia"]
+        "country", "Country", choices=["Spain", "Ecolandia", "Petrolandia"]
     ),
 
     ui.input_select(
-        "entity", "Machine", choices=["Magnito", "GravityGuy"]
+        "entity", "Machine", choices=["SE DWIMVb1000s/mm2 2.0mm", "Magnito", "GravityGuy"] 
     ),
 
     ui.input_select(
         "modality", "Modality", choices=["MRI"]
     ),
 
-    ui.output_text_verbatim("text"), 
+    ui.output_text_verbatim("consumption"), 
 
     )
 
@@ -32,9 +33,27 @@ app_ui = ui.page_fluid(
 # Server function provides access to client-side input values
 def server(input):
     @render.text  
-    def text():
-        return input.minutes()
-    
+    def consumption(modality = input.modality, entity = input.entity, duration = input.minutes, country = input.country, year = input.year):
+
+
+        modality = modality.get()
+        entity = entity.get()
+        duration = duration.get()
+        country = country.get()
+        year= year.get()
+
+        df_carbon = pd.read_csv("data/carbon-intensity.csv")
+        df_energy = pd.read_csv("data/Chodorowski_energy.csv")
+
+        mask = (df_carbon["Entity"] == country) & (df_carbon["Year"] == year)
+        carbon_intensity = df_carbon.loc[mask, "Carbon intensity of electricity - gCO2/kWh"]
+
+        kWh_per_minute   = df_energy[df_energy["sequence"] == entity]["kWh_per_minute"]
+        consumption = float(duration) * kWh_per_minute.iloc[0] * carbon_intensity.iloc[0]
+
+        return consumption 
+ 
+ 
 app = App(app_ui, server)
 
 if __name__ == "__main__":
