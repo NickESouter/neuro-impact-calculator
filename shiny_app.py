4b7287b9709407911677ed43dc548b90c48a6ace
clelf <clementinelvfd@gmail.com>
2025-11-21 16:24:49 +0100

    Text rendered, sample size, conversion to flights and cars

--- shiny_app.py
+++ shiny_app.py
@@ -39,44 +39,66 @@ def load_scanner_data(scannerData_filename=scannerData_filename):
 
     return df_models
 
-def compute_percents(summary, transport_mode):
-    # TODO: implement
-    return 0
+def compute_transports_comparison(carbon_emissions_g):
+    # COMPARISON DATA
+    flight_short = 221.8 # kg of co2, london to paris return
+    flight_long = 1900 # kg of co2, london to NYC return
+    car_ratio_km = 106.4 # g of co2 / km
+    car_ratio_mile = 171 # g of co2 / mile
+
+    # carbon emissions in kg
+    ce_kg = convert_g2kg(carbon_emissions_g)
+
+    # Transportation comparison summary
+    transports_data = {}
+
+    # Compute flight comparison
+    if ce_kg / flight_short < 1:
+        transports_data['flight_percent'] = (ce_kg / flight_short) * 100
+        transports_data['flight_type'] = 'London-Paris return flight'
+    else:
+        transports_data['flight_percent'] = (ce_kg / flight_long) * 100
+        transports_data['flight_type'] = 'London-New York return flight'
+
+    # Compute car comparison
+    transports_data['car_dist_km'] = carbon_emissions_g / car_ratio_km
+    transports_data['car_dist_mile'] = carbon_emissions_g / car_ratio_mile
+
+    return transports_data
 
 def convert_g2kg(grams):
     return grams / 1000.0
 
 def get_statement(summary):
+    parts = []
     if summary["year"] != summary["year_eff"]:
-        year_text = f"We don't have data for {summary['year']} yet, so the estimation provided is computed based on the closest year available ({summary['year_eff']}) for the country selected, {summary['country']}.\n"
-    else:
-        year_text = ""
-    
+        parts.append(f"<p><em>We don't have data for {summary['year']} yet, so the estimation provided is computed based on the closest year available ({summary['year_eff']}) for the country selected, {summary['country']}.</em></p>")
     if summary["model"] == "Other":
-        model_text = f"The value provided below is computed as the median for {summary['field_strength']:.1f}T MRI models in our database.\n"
+        parts.append(f"<p><em>The value provided below is computed as the median for {summary['field_strength']:.1f}T MRI models in our database.</em></p>")
     else:
-        model_text = f"You have selected the {summary['model']} model.\n"
+        parts.append(f"<p><em>You have selected the {summary['model']} model.</em></p>")
 
-    text = year_text + model_text + (
-        f"For the current study, {summary['scan_power']:.2f} kWh was used for MRI scanning for a duration of active scanning of {summary['scan_duration']:.0f} minutes "
-        f"and an additional {summary['idle_power']:.2f} kWh for idle scanning for a duration of {summary['idle_duration']:.0f} minutes, "
-        f"and {summary['computing_energy']:.2f} kWh for data processing and analysis.\n"
-        f"In {summary['country']} in {summary['year_eff']}, with a carbon intensity value of {summary['carbon_intensity']:.2f} grams of carbon dioxide per kWh (gCO2/kWh), "
-        f"this amounted to {convert_g2kg(summary['carbon_emissions']):.2f} kilograms of carbon dioxide-equivalent emissions.\n"
-        f"This is equivalent to {compute_percents(summary, 'flight'):.2f}% of a return flight from London to Paris, "
-        f"{compute_percents(summary, 'car'):.2f}% miles driven in a passenger car."
-    )
-    return text
+    parts.append(f"<p>For the current study, <strong>{summary['scan_power']:.2f} kWh</strong> was used for MRI scanning <strong>{summary['sample_size']}</strong> subjects for a duration of active scanning of <strong>{summary['scan_duration']:.0f} minutes</strong> per subject,<br>"
+                 f"and an additional <strong>{summary['idle_power']:.2f} kWh</strong> for idle scanning for a duration of <strong>{summary['idle_duration']:.0f} minutes</strong> per subject,<br>"
+                 f"and <strong>{summary['computing_energy']:.2f} kWh</strong> for data processing and analysis.</p>")
 
-def compute_scan(modality, model, field_strength, scan_duration, idle_duration, country, year, scannerData_filename=scannerData_filename, countryCarbonIntensity_filename=countryCarbonIntensity_filename):
+    parts.append(f"<p>In <strong>{summary['country']}</strong> in <strong>{summary['year_eff']}</strong>, with a carbon intensity value of <strong>{summary['carbon_intensity']:.2f} grams</strong> of carbon dioxide per kWh (gCO2/kWh),<br>"
+                 f"this amounted to <strong>{convert_g2kg(summary['carbon_emissions']):.2f} kilograms</strong> of carbon dioxide-equivalent emissions.</p>")
+
+    parts.append(f"<p>This is equivalent to <strong>{summary['transports_data']['flight_percent']:.2f}%</strong> of a <strong>{summary['transports_data']['flight_type']}</strong>,"
+                 f"<strong>{summary['transports_data']['car_dist_km']:.2f} kilometers</strong> or <strong>{summary['transports_data']['car_dist_mile']:.2f} miles</strong> driven in a passenger car.</p>")
+
+    return "\n".join(parts)
+
+def compute_scan(modality, model, field_strength, scan_duration, idle_duration, country, year, sample_size, scannerData_filename=scannerData_filename, countryCarbonIntensity_filename=countryCarbonIntensity_filename):
     
     # Country specific data
     df_carbon = pd.read_csv(countryCarbonIntensity_filename)
 
     # If the year selected is not in the data we have for the selected country, take closest available year
     if year not in df_carbon[df_carbon["Entity"] == country]["Year"].values:
-        available_years = sorted(df_carbon[df_carbon["Entity"] == country]["Year"].unique())
-        year_eff = available_years[-1] # (abs(available_years - year)).argmin()
+        # available_years = sorted(df_carbon[df_carbon["Entity"] == country]["Year"].unique())
+        # year_eff = available_years[-1] # (abs(available_years - year)).argmin()
         available_years = df_carbon[df_carbon["Entity"] == country]["Year"].unique()
         year_eff = available_years[(abs(available_years - year)).argmin()]
     else:
@@ -113,18 +135,28 @@ def compute_scan(modality, model, field_strength, scan_duration, idle_duration, 
                 raise ValueError(f"No idle_mode entries for field strength {field_strength}")
             idle_power = idle_mode_vals.median() # More robust than the mean given observed data
         
-        carbon_emissions = carbon_intensity * mri_consumption(idle_power, scan_power, scan_duration, idle_duration)
+        # Total active scan and idle duration times
+        scan_duration_tot = scan_duration * sample_size
+        idle_duration_tot = idle_duration * sample_size
+
+        # Calculate carbon emissions
+        carbon_emissions = carbon_intensity * mri_consumption(idle_power, scan_power, scan_duration_tot, idle_duration_tot)
 
         # COMPUTING-RELATED CALCULATIONS
         ################################
 
         computing_energy = computing_consumption(cpu_hours=2, ram_gb=32, gpu_hours=0, pue_hpc = 1.56)
 
+        # COMPUTE PERCENTAGES
+        #####################
+        transports_data = compute_transports_comparison(carbon_emissions)
+
 
         # SUMMARY
         #########
 
         summary = {
+            "sample_size": sample_size,
             "country": country,
             "year": year,
             "year_eff": year_eff,
@@ -136,20 +168,23 @@ def compute_scan(modality, model, field_strength, scan_duration, idle_duration, 
             "carbon_emissions": carbon_emissions,
             "scan_power": scan_power,
             "idle_power": idle_power,
-            "computing_energy": computing_energy
+            "computing_energy": computing_energy,
+            "transports_data": transports_data
         }
 
         return summary
 
+
+# Server function provides access to client-side input values
 def server(input, output, session):
-        @render.ui
-        def model_ui():
-            field_strength = input.field_strength.get()
-            if field_strength is None or field_strength == "":
-                choices = get_choices(scannerData_filename, "model_full", other=True)
-            else:
-                choices = get_choices(scannerData_filename, "model_full", filter_cat="Field strength", filter_val=float(field_strength), other=True)
-            return ui.input_select("model", "Model", choices=choices)
+    @render.ui
+    def model_ui():
+        field_strength = input.field_strength.get()
+        if field_strength is None or field_strength == "":
+            choices = get_choices(scannerData_filename, "model_full", other=True)
+        else:
+            choices = get_choices(scannerData_filename, "model_full", filter_cat="Field strength", filter_val=float(field_strength), other=True)
+        return ui.input_select("model", "Model", choices=choices)
 
         @render.image
         def logo():
@@ -167,11 +202,25 @@ def server(input, output, session):
             idle_duration = float(input.idle_duration.get()) * sample_size
             country = input.country.get()
             year = input.year.get()
+    @render.ui
+    def consumption(scannerData_filename=scannerData_filename, 
+                    countryCarbonIntensity_filename=countryCarbonIntensity_filename,
+                    input=input):
+        # Read inputs from Shiny input objects
+        modality = input.modality.get()
+        model = input.model.get()
+        sample_size = input.sample_size.get()
+        field_strength = float(input.field_strength.get())
+        scan_duration = float(input.scan_duration.get())
+        idle_duration = float(input.idle_duration.get())
+        country = input.country.get()
+        year = input.year.get()
 
-            try:
-                return get_statement(compute_scan(modality, model, field_strength, scan_duration, idle_duration, country, year, scannerData_filename=scannerData_filename, countryCarbonIntensity_filename=countryCarbonIntensity_filename))
-            except Exception as e:
-                return f"Error: {e}"
+        try:
+            html_text = get_statement(compute_scan(modality, model, field_strength, scan_duration, idle_duration, country, year, sample_size, scannerData_filename=scannerData_filename, countryCarbonIntensity_filename=countryCarbonIntensity_filename))
+            return ui.HTML(html_text)
+        except Exception as e:
+            return ui.HTML(f"<em>Error: {e}</em>")
 
 if __name__ == "__main__":
 
@@ -187,9 +236,9 @@ if __name__ == "__main__":
         ui.layout_columns(
             # Sidebar (left panel) for inputs
             ui.card(
-                ui.input_numeric("scan_duration", "Duration of active scanning (in minutes)", 60), 
-                ui.input_numeric("idle_duration", "Duration of idle scanning (in minutes)", 15), 
-                ui.input_numeric("sample_size", "Sample size", 1), 
+                ui.input_numeric("sample_size", "Number of subjects scanned", 1, min=1),
+                ui.input_numeric("scan_duration", "Duration of active scanning per subject (in minutes)", 60), 
+                ui.input_numeric("idle_duration", "Duration of idle scanning per subject (in minutes)", 15), 
                 ui.input_select(
                     "country", "Country", choices=get_choices("data/carbon-intensity.csv", "Entity")
                 ),
@@ -205,8 +254,9 @@ if __name__ == "__main__":
 
             # Main panel (right) for output
             ui.card(
-                ui.output_text("consumption"),
-            ),
+                ui.HTML(ui.output_ui("consumption"), ),
+                position="right"
+            )
         )
     )
 
